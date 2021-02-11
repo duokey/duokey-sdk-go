63382c664e47a2efc96a22211ff8f54ddc2b435e
Jean-Luc Beuchat <jean-luc.beuchat@hevs.ch>
2021-02-11 12:53:18 +0000

    Send request

--- duokey/request/request.go
+++ duokey/request/request.go
@@ -1,20 +1,26 @@
 package request
 
 import (
-	_ "io"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"net/url"
+	"path"
 
 	"github.com/duokey/duokey-sdk-go/duokey"
+	"github.com/pkg/errors"
 )
 
 // Request ...
 type Request struct {
-
-	HTTPRequest            *http.Request
-	HTTPResponse           *http.Response
-	Parameters interface{}
-	Response interface{}
-
+	HTTPClient   *http.Client
+	HTTPRequest  *http.Request
+	HTTPResponse *http.Response
+	Error        error
+	Parameters   interface{}
+	Data         interface{}
 }
 
 // Operation (GET, POST)
@@ -24,10 +30,8 @@ type Operation struct {
 	HTTPPath   string
 }
 
-
 // https://golang.org/pkg/net/http/#NewRequest
 // func NewRequestWithContext(ctx context.Context, method, url string, body io.Reader) (*Request, error)
-
 
 // func (c *Client) NewRequest(operation *request.Operation, params interface{}, data interface{}) *request.Request {
 //	return request.New(c.Config, c.ClientInfo, c.Handlers, c.Retryer, operation, params, data)
@@ -43,22 +47,68 @@ func New(config duokey.Config, operation *Operation, params interface{}, data in
 	default:
 		method = http.MethodPost
 	}
-	
+
 	httpReq, _ := http.NewRequest(method, "", nil)
 
+	rawurl := path.Join(config.Endpoint, operation.HTTPPath)
+
+	var err error
+	httpReq.URL, err = url.Parse(rawurl)
+	if err != nil {
+		httpReq.URL = &url.URL{}
+		err = fmt.Errorf("InvalidEndpointURL (%s)", rawurl)
+	}
+
 	r := &Request{
+		HTTPClient:  config.HTTPClient,
 		HTTPRequest: httpReq,
+		Error:       err,
+		Parameters:  params,
+		Data:        data,
 	}
 
 	return r
 }
 
-
-
 // Send ...
 func (r *Request) Send() error {
 
-	
+	body := &bytes.Buffer{}
+	if r.Parameters != nil {
+		if err := json.NewEncoder(body).Encode(r.Parameters); err != nil {
+			return errors.Wrap(err, "failed to serialize request body")
+		}
+	}
+
+	r.HTTPRequest.Body = ioutil.NopCloser(body)
+
+	var err error
+	if r.HTTPResponse, err = r.HTTPClient.Do(r.HTTPRequest); err != nil {
+		return errors.Wrap(err, "failed make HTTP request")
+	}
+
+	if err = parseHTTPResponse(r.HTTPResponse, r.Data); err != nil {
+		return errors.Wrap(err, "failed to parse HTTP response")
+	}
+
+	return nil
+}
+
+func parseHTTPResponse(resp *http.Response, response interface{}) error {
+	defer resp.Body.Close()
+
+	var payload []byte
+	var err error
+
+	// TODO: check error code (resp.StatusCode)
+
+	if payload, err = ioutil.ReadAll(resp.Body); err != nil {
+		return errors.Wrap(err, "failed to read response body")
+	}
+
+	if err = json.NewDecoder(bytes.NewReader(payload)).Decode(response); err != nil {
+		return errors.Wrap(err, "failed to decode response body")
+	}
 
 	return nil
 }
